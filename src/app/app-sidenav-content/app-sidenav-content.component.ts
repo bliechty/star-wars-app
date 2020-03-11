97f7f9956cc44e1825288f21cf2f9fbab718c7c9
Brady Liechty <berrybloxinator@gmail.com>
2020-03-11 09:42:30 -0600

    initial commit

---
+++ src/app/app-sidenav-content/app-sidenav-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-content',
+  templateUrl: './app-sidenav-content.component.html',
+  styleUrls: ['./app-sidenav-content.component.css']
+})
+export class AppSidenavContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppCharacterListComponent } from './app-character-list/app-character-list.component';
+import { AppSidenavContentComponent } from './app-sidenav-content/app-sidenav-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppCharacterListComponent,
+    AppSidenavContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
